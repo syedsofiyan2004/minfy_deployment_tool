3591c053d61dc0cd3d30eb13b489c237a227b44b
syedsofiyan2004 <syed.sofiyan@minfytech.com>
2025-07-20 23:12:32 +0530

    final updated pipeline

--- minfy-cli/src/minfy/commands/auth.py
+++ minfy-cli/src/minfy/commands/auth.py
@@ -5,13 +5,18 @@ import boto3
 from ..config import AWSAuth, save_global
 
 @click.command("auth")
-def auth_cmd():
+@click.option("--aws-access-key-id", default=None, help="AWS Access Key ID")
+@click.option("--aws-secret-access-key", default=None, help="AWS Secret Access Key")
+@click.option("--aws-session-token", default=None, help="AWS Session Token (optional)")
+@click.option("--region", default=None, help="Default AWS region")
+@click.option("--profile", default=None, help="AWS Profile name (optional)")
+def auth_cmd(aws_access_key_id, aws_secret_access_key, aws_session_token, region, profile):
     click.echo("Enter IAM user credentials with S3 and EC2 permissions.")
-    key_id = click.prompt("AWS Access Key ID")
-    secret_key = getpass.getpass("AWS Secret Access Key: ")
-    session_token = click.prompt("AWS Session Token (optional)", default="", show_default=False) or None
-    region = click.prompt("Default AWS region", default="ap-south-1")
-    profile = click.prompt("AWS Profile name (optional)", default="", show_default=False) or None
+    key_id = aws_access_key_id or click.prompt("AWS Access Key ID")
+    secret_key = aws_secret_access_key or getpass.getpass("AWS Secret Access Key: ")
+    session_token = aws_session_token or click.prompt("AWS Session Token (optional)", default="", show_default=False) or None
+    region = region or click.prompt("Default AWS region", default="ap-south-1")
+    profile = profile or click.prompt("AWS Profile name (optional)", default="", show_default=False) or None
     try:
         test_creds = {
             "aws_access_key_id": key_id,
